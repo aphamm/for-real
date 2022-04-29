75c6264c92887a6249036726e3f0b830146fb616
George <Georgimama111@gmail.com>
2022-04-28 21:07:14 -0700

    first commit

---
+++ src/frontend/Screens/Feed.js
@@ -0,0 +1,80 @@
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Post from '../components/Post';
+
+export default function Feed() {
+  const dummyData = [
+    {
+      user: 'Thomas5236',
+      answer: 'Fosizzle',
+      number: 4,
+    },
+    {
+      user: 'George1234',
+      answer: 'This is a dumbie answer',
+      number: 0,
+    },
+    {
+      user: 'Angelina02184',
+      answer: 'He touching the grass',
+      number: 2,
+    },
+    {
+      user: 'Sam20954',
+      answer: 'Keep him from your daughters',
+      number: -3,
+    },
+    {
+      user: 'Eggert294',
+      answer: 'Guy is a menace',
+      number: 1,
+    },
+    {
+      user: 'Eggert294',
+      answer: 'Guy is a menace',
+      number: 1,
+    },
+    {
+      user: 'Eggert294',
+      answer: 'Guy is a menace',
+      number: 1,
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.questionofday}>
+        <Text style={styles.question}>Is Austin Pham down bad?</Text>
+      </View>
+      <FlatList
+        data={dummyData}
+        renderItem={(item) => {
+          console.log(item);
+          return (
+            <Post
+              user={item.item.user}
+              answer={item.item.answer}
+              number={item.item.number}
+              keyExtractor={(item) => item.user}
+            />
+          );
+        }}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  question: {
+    fontSize: 20,
+  },
+  questionofday: {
+    backgroundColor: '#D3D3D3',
+    width: '100%',
+    padding: 40,
+    borderRadius: 14,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  container: {},
+});
