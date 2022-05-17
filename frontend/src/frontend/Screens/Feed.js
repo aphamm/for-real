12b0af27c4f68b461e20c952dd829693e0d2b7ff
AngieLue123 <angiel8686@gmail.com>
2022-05-17 01:18:39 -0700

    proper commit with changes, other commit didn't work

--- frontend/src/frontend/Screens/Feed.js
+++ frontend/src/frontend/Screens/Feed.js
@@ -1,26 +1,26 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ImageBackground } from 'react-native';
 import Post from '../components/Post';
 
 export default function Feed() {
   const dummyData = [
     {
       user: 'Thomas5236',
-      answer: 'Fosizzle',
+      answer: 'Def 5 milli who cares bout Jay Z get a bag and run wit it am i rite',
       number: 4,
     },
     {
       user: 'George1234',
-      answer: 'This is a dumbie answer',
+      answer: 'neither, both are overrated',
       number: 0,
     },
     {
       user: 'Angelina02184',
-      answer: 'He touching the grass',
+      answer: 'I would rather have dinner with Jay Z because what are you actually gonna do with 5 mil',
       number: 2,
     },
     {
       user: 'Sam20954',
-      answer: 'Keep him from your daughters',
+      answer: 'can i do lunch with jay z and get $1 million instead?',
       number: -3,
     },
     {
@@ -40,11 +40,26 @@ export default function Feed() {
     },
   ];
 
+  const image = {uri: "https://i.pinimg.com/736x/41/33/f9/4133f987e7712ec45394bb2bf9204002.jpg"}
   return (
     <View style={styles.container}>
-      <View style={styles.questionofday}>
-        <Text style={styles.question}>Is Austin Pham down bad?</Text>
+      <ImageBackground 
+        source={image}
+        style={{width: '100%', height: '100%', position: 'absolute', opacity: 0.8}}>
+      </ImageBackground>
+      {/* <Image 
+        source={require("../../assets/tempgrad.png/")}
+        style={styles.bgimage}>
+      </Image> */}
+      <View style={styles.header}>
+        <View style={styles.menu}>
+          <Text style={styles.menuItem}>Global</Text>
+          <Text style={styles.menuItem}>Your Community</Text>
+          <Text style={styles.menuItem}>Profile</Text>
+        </View>
+        <Text style={styles.question}>Would you rather have $5 million or dinner with Jay Z and why?</Text>
       </View>
+
       <FlatList
         data={dummyData}
         renderItem={(item) => {
@@ -64,17 +79,35 @@ export default function Feed() {
 }
 const styles = StyleSheet.create({
   question: {
-    fontSize: 20,
+    marginTop: 30,
+    fontSize: 15,
+    textAlign: 'center',
+    paddingLeft: 35,
+    paddingRight: 35,
+    
   },
-  questionofday: {
-    backgroundColor: '#D3D3D3',
-    width: '100%',
-    padding: 40,
-    borderRadius: 14,
+  header: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 20,
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 50,
+    marginBottom: 10,
   },
-  container: {},
+  container: {
+     backgroundColor: 'white',
+  },
+  menu: {
+    marginTop: 40,
+    alignContent: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  menuItem: {
+    marginHorizontal: 15,
+    textDecorationLine: 'underline'
+
+  },
+  
 });
--- frontend/src/frontend/components/Post.js
+++ frontend/src/frontend/components/Post.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function Post(props) {
   function getColor(num) {
     if (num > 0) {
@@ -21,47 +22,29 @@ export default function Post(props) {
 
   return (
     <View style={styles.container}>
-      <View style={styles.leftContainer}>
-        <Text style={styles.username}>{props.user}</Text>
+      <View style={styles.textContainer}>
         <Text style={styles.answer}>{props.answer}</Text>
+        <View style={styles.userandtime}>
+          <Text style={styles.username}>{props.user}</Text>
+          <Text style={styles.username}> - </Text>
+          <Text style={styles.time}>5 mins ago</Text>
+        </View>
       </View>
-      <View style={styles.rightContainer}>
-        <Text>Up</Text>
-        <Text style={[styles.number, getColor(props.number)]}>
-          {props.number}
-        </Text>
-        <Text>Down</Text>
+      
+      <View style={styles.numberContainer}>
+        <Text>∧</Text>
+        <Text style={[styles.number, getColor(props.number)]}>{props.number}</Text>
+        <Text>∨</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  number: {
-    fontSize: 35,
-    margin: 10,
-  },
-  answer: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  username: {
-    marginBottom: 30,
-    fontWeight: 'bold',
-    fontSize: 15,
-  },
-  rightContainer: {
-    flex: 2,
-    padding: 6,
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  leftContainer: {
-    flex: 8,
-  },
+  
+  
   container: {
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
     display: 'flex',
@@ -69,5 +52,38 @@ const styles = StyleSheet.create({
     padding: 30,
     borderColor: 'grey',
     borderBottomWidth: 1,
+  },
+  textContainer: {
+    flex: 8,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numberContainer: {
+    flex: 2.5,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  userandtime: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  number: {
+    fontSize: 26,
+  },
+  answer: {
+    fontSize: 14,
+    marginBottom: 15,
+  },
+  username: {
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  time: {
+    fontSize: 10,
+    color: 'grey',
   },
 });
