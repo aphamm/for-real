7d130f48ec8c2d3484f7c7ecdcf94c2fe2e2c39c
George <Georgimama111@gmail.com>
2022-05-17 23:23:24 -0700

    making it so that navigator is component in appjs

--- frontend/App.js
+++ frontend/App.js
@@ -3,9 +3,7 @@ import Navigator from './src/frontend/routes/NavStack';
 import Feed from './src/frontend/Screens/Feed';
 
 export default function App() {
-  return (
-    <Feed />
-  );
+  return <Navigator />;
 }
 
 const styles = StyleSheet.create({});
