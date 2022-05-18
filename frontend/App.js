a9035ba9dbdcd2cd603a1bf962419904d58aca2e
AngieLue123 <angiel8686@gmail.com>
2022-05-17 23:06:32 -0700

    Pushing all small changes

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Navigator from './src/frontend/routes/NavStack';
+import Feed from './src/frontend/Screens/Feed';
 
 export default function App() {
   return (
-    <Navigator />
+    <Feed />
   );
 }
 
