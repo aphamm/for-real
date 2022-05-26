030cebbc7221beb14dbc9ea27d163a0bc3c15471
Samuel Li <samuelli@s-169-232-117-157.resnet.ucla.edu>
2022-05-26 02:54:03 -0700

    Now app will actually start up in profile page

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,10 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Navigator from './src/frontend/routes/NavStack';
 import Feed from './src/frontend/Screens/Feed';
+import Profile from './src/frontend/Screens/Profile';
 
 export default function App() {
-  return <Navigator />;
+  return <Profile />;
 }
 
 const styles = StyleSheet.create({});
